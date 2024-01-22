cfd82d00bba2b7613a352a1a13bc22bf36529e8f
nini15 <126186307+nini15@users.noreply.github.com>
2024-01-22 14:50:06 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var model=document.getElementsByClassName("container")
+model[0].innerHTML= "<b>1</b>"
+model[1].innerHTML= "<b>2</b>"
+model[2].innerHTML= "<b>3</b>"
+model[3].innerHTML= "<b>4</b>"
+model[4].innerHTML= "<b>5</b>"
